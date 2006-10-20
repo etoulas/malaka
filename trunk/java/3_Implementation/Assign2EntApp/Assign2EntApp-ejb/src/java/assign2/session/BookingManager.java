765c81b2e230c48460508a721c69e270cd06a7d7
kai.ramuenke <kai.ramuenke@d14053f0-d41e-0410-8e62-47ffcaada4dd>
2006-10-20 05:23:38 +0000

    initial version

---
+++ trunk/java/3_Implementation/Assign2EntApp/Assign2EntApp-ejb/src/java/assign2/session/BookingManager.java
@@ -0,0 +1,37 @@
+/*
+ * ManageBookingsBean.java
+ *
+ * Created on October 20, 2006, 12:52 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package assign2.session;
+
+import assign2.entities.Booking;
+import assign2.entities.to.BookingDetailsTO;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author ramu01
+ */
+@Stateless
+public class BookingManager implements assign2.session.BookingManagerRemote, assign2.session.BookingManagerLocal {
+    
+    @PersistenceContext
+    EntityManager em;
+    
+    /** Creates a new instance of ManageBookingsBean */
+    public BookingManager() {
+        
+    }
+    
+    public void createBooking(BookingDetailsTO bookingTO) {
+        Booking booking = new Booking(bookingTO);
+        em.persist(booking);
+    }
+}
