144aaa896bea0600cfabf9371ee73505fa7af6f6
kai.ramuenke <kai.ramuenke@d14053f0-d41e-0410-8e62-47ffcaada4dd>
2006-10-21 10:41:47 +0000

    added more getters and setters

--- java/3_Implementation/Assign2EntApp-ejb/src/java/assign2/entities/to/AddressTO.java
+++ java/3_Implementation/Assign2EntApp-ejb/src/java/assign2/entities/to/AddressTO.java
@@ -15,10 +15,82 @@ package assign2.entities.to;
  */
 public class AddressTO
 {
+    private Long id;
+    private Integer postCode;
+    private String countryState;
+    private String city;
+    private String country;
+    private String streetNumber;
+    private String street;
+    private String suburb;
     
     /** Creates a new instance of AddressTO */
     public AddressTO()
     {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(Integer postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCountryState() {
+        return countryState;
+    }
+
+    public void setCountryState(String countryState) {
+        this.countryState = countryState;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
     }
     
 }
