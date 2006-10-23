7c6e5d56abb219f229f19e11446b963c4ce21f1a
ggotti121 <ggotti121@d14053f0-d41e-0410-8e62-47ffcaada4dd>
2006-10-23 04:47:40 +0000

    Added a license and vehicle type field.

--- java/3_Implementation/Assign2EntApp-ejb/sql/defaultData.sql
+++ java/3_Implementation/Assign2EntApp-ejb/sql/defaultData.sql
@@ -1,2 +1,15 @@
 INSERT INTO BookingType VALUES (1, 'Transfer');
 INSERT INTO BookingType VALUES (2, 'Hourly');
+
+--Delete all old Data
+DELETE FROM VEHICLETYPE;
+DELETE FROM VEHICLELICENSE;
+
+--Insert values into VehicleTypes
+INSERT INTO VEHICLETYPE VALUES('SEDAN','A standard luxury sedan',4);
+INSERT INTO VEHICLETYPE VALUES('LIMO6','A six seater limo',6);
+INSERT INTO VEHICLETYPE VALUES('LIMO8','A eight seater limo',8);
+
+--Insert values for License
+INSERT INTO VEHICLELICENSE VALUES(1,'HV Licence');
+INSERT INTO VEHICLELICENSE VALUES(2,'Taxi Licence');
