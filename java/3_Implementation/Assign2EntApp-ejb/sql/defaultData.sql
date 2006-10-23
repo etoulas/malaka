81ac3a2fc8734bf3880eb9a170ed74157af21cfc
efthimios.toulas <etoulas@users.noreply.github.com>
2006-10-23 05:15:23 +0000

    added SQL for all fixed data like *Type and *License

--- java/3_Implementation/Assign2EntApp-ejb/sql/defaultData.sql
+++ java/3_Implementation/Assign2EntApp-ejb/sql/defaultData.sql
@@ -1,15 +1,37 @@
+-- drop tables from sample db
+DROP TABLE Sequence;
+DROP TABLE Micro_Markets;
+DROP TABLE Orders;
+DROP TABLE Product;
+DROP TABLE Product_Code;
+DROP TABLE Customer;
+DROP TABLE Discount_Code;
+DROP TABLE Manufacture;
+
+-- delete all old Data
+DELETE FROM BookingType;
+DELETE FROM EventType;
+DELETE FROM VehicleType;
+DELETE FROM VehicleLicense;
+DELETE FROM DriversLicense;
+
+-- insert booking types
 INSERT INTO BookingType VALUES (1, 'Transfer');
 INSERT INTO BookingType VALUES (2, 'Hourly');
 
---Delete all old Data
-DELETE FROM VEHICLETYPE;
-DELETE FROM VEHICLELICENSE;
+-- insert event types
+INSERT INTO EventType VALUES (1, 'Wedding/Party/Special event');
+INSERT INTO EventType VALUES (2, 'Other (drop off/pick up)');
 
---Insert values into VehicleTypes
-INSERT INTO VEHICLETYPE VALUES('SEDAN','A standard luxury sedan',4);
-INSERT INTO VEHICLETYPE VALUES('LIMO6','A six seater limo',6);
-INSERT INTO VEHICLETYPE VALUES('LIMO8','A eight seater limo',8);
+-- insert vehicle types
+INSERT INTO VehicleType VALUES ('SEDAN', 'A standard luxury sedan', 4);
+INSERT INTO VehicleType VALUES ('LIMO6', 'A six seater limo, fully a/c', 6);
+INSERT INTO VehicleType VALUES ('LIMO8', 'A eight seater limo, fully a/c, bar and fridge', 8);
 
---Insert values for License
-INSERT INTO VEHICLELICENSE VALUES(1,'HV Licence');
-INSERT INTO VEHICLELICENSE VALUES(2,'Taxi Licence');
+-- insert vehicle licenses
+INSERT INTO VehicleLicense VALUES (1, 'SV License');
+INSERT INTO VehicleLicense VALUES (2, 'VH License');
+
+-- insert drivers licenses
+INSERT INTO DriversLicense VALUES (1, 'HV License');
+INSERT INTO DriversLicense VALUES (2, 'Taxi License');
