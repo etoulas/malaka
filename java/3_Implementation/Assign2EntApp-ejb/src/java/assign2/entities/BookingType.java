9f070338559b031edc8e71c9fadd3f02ddb1a360
kai.ramuenke <kai.ramuenke@d14053f0-d41e-0410-8e62-47ffcaada4dd>
2006-10-23 06:01:21 +0000

    added named queries

--- java/3_Implementation/Assign2EntApp-ejb/src/java/assign2/entities/BookingType.java
+++ java/3_Implementation/Assign2EntApp-ejb/src/java/assign2/entities/BookingType.java
@@ -9,22 +9,32 @@
 
 package assign2.entities;
 
+import assign2.entities.to.BookingTypeTO;
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Column;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  * Entity class BookingType
- * 
+ *
  * @author Gerard Gigliotti
  */
 @Entity
-public class BookingType implements Serializable
-{
-
+@NamedQueries({
+    @NamedQuery(
+    name="findAllBookingTypes",
+            query="SELECT bt FROM BookingType bt"),
+    @NamedQuery(
+    name="findBookingTypeByID",
+            query="SELECT bt FROM BookingType bt WHERE bt.id = :id")
+})
+public class BookingType implements Serializable {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -32,52 +42,52 @@ public class BookingType implements Serializable
     private String description;
     
     /** Creates a new instance of BookingType */
-    public BookingType()
-    {
+    public BookingType() {
     }
-
+    
+    public BookingType(BookingTypeTO to) {
+        this.setId(to.getId());
+        this.setDescription(to.getDescription());
+    }
+    
     /**
      * Gets the id of this BookingType.
      * @return the id
      */
-    public Long getId()
-    {
+    public Long getId() {
         return this.id;
     }
-
+    
     /**
      * Sets the id of this BookingType to the specified value.
      * @param id the new id
      */
-    public void setId(Long id)
-    {
+    public void setId(Long id) {
         this.id = id;
     }
-
+    
     /**
-     * Returns a hash code value for the object.  This implementation computes 
+     * Returns a hash code value for the object.  This implementation computes
      * a hash code value based on the id fields in this object.
      * @return a hash code value for this object.
      */
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         int hash = 0;
         hash += (this.id != null ? this.id.hashCode() : 0);
         return hash;
     }
-
+    
     /**
-     * Determines whether another object is equal to this BookingType.  The result is 
-     * <code>true</code> if and only if the argument is not null and is a BookingType object that 
+     * Determines whether another object is equal to this BookingType.  The result is
+     * <code>true</code> if and only if the argument is not null and is a BookingType object that
      * has the same id field values as this object.
      * @param object the reference object with which to compare
      * @return <code>true</code> if this object is the same as the argument;
      * <code>false</code> otherwise.
      */
     @Override
-    public boolean equals(Object object)
-    {
+    public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof BookingType)) {
             return false;
@@ -86,26 +96,31 @@ public class BookingType implements Serializable
         if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) return false;
         return true;
     }
-
+    
     /**
-     * Returns a string representation of the object.  This implementation constructs 
+     * Returns a string representation of the object.  This implementation constructs
      * that representation based on the id fields.
      * @return a string representation of the object.
      */
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "assign2.bookings.BookingType[id=" + id + "]";
     }
-
-    public String getDescription()
-    {
+    
+    public String getDescription() {
         return description;
     }
-
-    public void setDescription(String description)
-    {
+    
+    public void setDescription(String description) {
         this.description = description;
+    }
+    
+    public BookingTypeTO getData() {
+        
+        BookingTypeTO to = new BookingTypeTO();
+        to.setId(this.getId());
+        to.setDescription(this.getDescription());
+        return to;
     }
     
 }
