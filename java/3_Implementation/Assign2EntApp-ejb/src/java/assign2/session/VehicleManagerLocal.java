d712b4b5ac6f4fa8ffb24208189112c6a4e85d17
ggotti121 <ggotti121@d14053f0-d41e-0410-8e62-47ffcaada4dd>
2006-10-26 04:40:23 +0000

    Modification to making a booking.

--- java/3_Implementation/Assign2EntApp-ejb/src/java/assign2/session/VehicleManagerLocal.java
+++ java/3_Implementation/Assign2EntApp-ejb/src/java/assign2/session/VehicleManagerLocal.java
@@ -28,4 +28,6 @@ public interface VehicleManagerLocal
     
     java.util.List<VehicleTO> getVehiclesByBookingType(BookingTypeTO bto);
 
+    assign2.entities.VehicleType getVehicleType(String code);
+
 }
