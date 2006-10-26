c50c375dc979076d5fa58e58fedc36d17fd9b5a6
kai.ramuenke <kai.ramuenke@d14053f0-d41e-0410-8e62-47ffcaada4dd>
2006-10-26 04:19:19 +0000

    initial version

---
+++ trunk/java/3_Implementation/Assign2EntApp-ejb/src/java/assign2/session/DriverManagerLocal.java
@@ -0,0 +1,13 @@
+
+package assign2.session;
+
+import javax.ejb.Local;
+
+
+/**
+ * This is the business interface for DriverManager enterprise bean.
+ */
+@Local
+public interface DriverManagerLocal {
+    
+}
