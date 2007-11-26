ecedbbdef695b1cf19a7808fcc1275b6b11a3a9c
efthimios.toulas <etoulas@users.noreply.github.com>
2007-11-26 10:42:43 +0000

    Erster Prolog Code

---
+++ prolog/run.pl
@@ -0,0 +1,79 @@
+/* , Teamsport, Einzel sport, Partner ben�tigt, Ausdauer, Kraft, Ballsport, Verletzungsgef., Zeitfakt., Koordination, Sommer, Winter, Outdoor, Indoor, Zuhause, Finanzaufw. 1x, Finanz. Contin., Familientaugl., Sozialer Asp., Pr�zision, Wettkampfbereitsch., Schwimmen, [Equipment] */
+
+/* gewichtung('Teamsport', [0.5, 1, 1.5]). */
+
+merkmale('Fussball', [ 3, 1, 1, 4, 2, 2, 3, 3, 3, 4, 3, 2, 2, 1, 2, 1, 1, 4, 3, 3, 1 ]).
+merkmale('Laufen', [ 1, 2, 1, 4, 2, 1, 2, 2, 1, 4, 2, 2, 1, 2, 2, 1, 3, 1, 1, 1, 1 ]).
+merkmale('Klettern', [ 1, 2, 2, 3, 4, 1, 3, 2, 3, 4, 2, 2, 2, 1, 2, 2, 3, 2, 1, 1, 1 ]).
+merkmale('Yoga', [ 1, 2, 1, 2, 2, 1, 1, 2, 3, 4, 4, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1 ]).
+merkmale('Radfahren', [ 1, 2, 1, 3, 2, 1, 2, 2, 1, 4, 3, 2, 2, 1, 3, 2, 4, 2, 1, 2, 1 ]).
+merkmale('Schwimmen', [ 1, 2, 1, 2, 1, 1, 1, 2, 1, 4, 4, 2, 2, 1, 1, 2, 4, 2, 1, 1, 2 ]).
+merkmale('Tennis', [ 2, 1, 2, 3, 2, 2, 2, 3, 3, 4, 4, 2, 2, 1, 2, 3, 2, 3, 3, 3, 1 ]).
+merkmale('Volleyball', [ 3, 1, 2, 2, 2, 2, 2, 3, 3, 4, 4, 1, 2, 1, 2, 2, 2, 4, 3, 3, 1 ]).
+merkmale('Basketball', [ 3, 1, 2, 3, 2, 2, 3, 3, 4, 4, 4, 2, 2, 2, 2, 1, 2, 4, 3, 3, 1 ]).
+merkmale('Handball', [ 3, 1, 2, 3, 3, 2, 3, 3, 4, 4, 4, 2, 2, 1, 2, 1, 1, 4, 3, 3, 1 ]).
+merkmale('Wasserball', [ 3, 1, 2, 4, 3, 2, 2, 3, 3, 4, 4, 2, 2, 1, 1, 1, 1, 4, 3, 3, 2 ]).
+merkmale('Tischtennis', [ 2, 1, 2, 2, 1, 2, 1, 2, 3, 4, 4, 2, 2, 2, 1, 1, 3, 2, 4, 3, 1 ]).
+merkmale('Squash', [ 1, 1, 2, 3, 3, 2, 3, 3, 3, 4, 4, 1, 2, 1, 2, 2, 2, 3, 4, 3, 1 ]).
+merkmale('Windsurfen', [ 1, 2, 1, 1, 2, 1, 1, 2, 2, 4, 1, 2, 1, 1, 3, 1, 1, 1, 1, 1, 2 ]).
+merkmale('Icehockey', [ 3, 1, 2, 3, 2, 2, 3, 3, 4, 1, 4, 2, 2, 1, 2, 2, 1, 3, 4, 3, 1 ]).
+merkmale('Skateboard', [ 1, 2, 1, 2, 1, 1, 3, 3, 4, 4, 1, 2, 2, 1, 1, 1, 1, 3, 1, 1, 1 ]).
+merkmale('Ballet', [ 1, 2, 2, 3, 2, 1, 1, 3, 4, 4, 4, 1, 2, 1, 1, 3, 1, 2, 3, 1, 1 ]).
+merkmale('Badminton', [ 2, 2, 2, 3, 1, 2, 2, 2, 3, 4, 4, 2, 2, 2, 2, 2, 4, 3, 3, 2, 1 ]).
+merkmale('Rugby', [ 3, 1, 2, 3, 3, 2, 4, 3, 3, 4, 2, 2, 1, 1, 2, 2, 1, 3, 3, 3, 1 ]).
+merkmale('Aerobic', [ 1, 2, 1, 3, 1, 1, 1, 2, 3, 4, 4, 2, 2, 2, 1, 2, 2, 2, 1, 1, 1 ]).
+merkmale('Gleitschirm/Fallschirm/Drachenfliegen', [ 1, 2, 1, 2, 2, 1, 3, 3, 3, 4, 1, 2, 1, 1, 4, 2, 2, 1, 1, 1, 1 ]).
+merkmale('Schach', [ 1, 1, 2, 1, 1, 1, 1, 2, 1, 4, 4, 2, 2, 2, 1, 1, 3, 2, 1, 2, 1 ]).
+merkmale('Segeln', [ 2, 1, 4, 2, 3, 1, 1, 3, 2, 4, 2, 4, 1, 1, 4, 3, 4, 4, 1, 2, 4 ]).
+merkmale('Schlittschufahren', [ 1, 4, 1, 2, 2, 1, 2, 2, 3, 3, 4, 2, 4, 1, 2, 2, 3, 2, 1, 2, 1 ]).
+merkmale('Inline', [ 1, 4, 1, 3, 2, 1, 3, 2, 3, 4, 2, 4, 2, 1, 2, 1, 4, 2, 1, 3, 1 ]).
+merkmale('Tanzen', [ 1, 1, 4, 3, 1, 1, 1, 2, 2, 4, 4, 3, 4, 2, 2, 1, 2, 3, 1, 1, 1 ]).
+merkmale('Boxen', [ 1, 1, 4, 3, 4, 1, 4, 3, 3, 4, 4, 1, 4, 1, 2, 2, 1, 2, 2, 4, 1 ]).
+merkmale('Eisstockschiessen', [ 3, 1, 4, 2, 1, 1, 1, 2, 2, 4, 4, 1, 4, 1, 2, 1, 1, 3, 4, 2, 1 ]).
+merkmale('Judo/Ju-Jutsu/Karate', [ 1, 1, 4, 3, 3, 1, 3, 3, 3, 4, 4, 1, 4, 2, 1, 2, 2, 3, 1, 4, 1 ]).
+merkmale('Taekwondo/Aikido', [ 1, 1, 4, 2, 2, 1, 3, 3, 3, 4, 4, 1, 4, 1, 2, 2, 2, 3, 2, 3, 1 ]).
+merkmale('Tai Ji', [ 1, 2, 1, 2, 1, 1, 1, 2, 4, 4, 4, 4, 4, 3, 2, 1, 1, 2, 1, 1, 1 ]).
+
+/* writeseq/1 gibt eine Liste in einer Zeile aus */
+writeseq([]).
+writeseq([A|B]) :- write(A), writeseq(B).
+
+ja_nein(Anfrage, Antwort) :-
+	repeat,
+	    write('(j/n) Antworten Sie mit "j." f�r JA oder mit "n." f�r NEIN: '), nl,
+	    writeseq([Anfrage, ' ']),
+	    read(L),
+	    (	L == j, Antwort = ja, !
+	     ;	L == n, Antwort = nein, !
+	     ;	nl, write('Falsche Eingabe.'), nl,
+		fail
+	    ).
+
+frage(schwimmen, 'K�nnen Sie schwimmen?').
+
+interview :-
+	frage(Context,Anfrage),
+	ja_nein(Anfrage, Antwort),
+	(   Antwort == ja,
+	    nl, writeseq([Context, ': ', Antwort])
+	 ;  fail
+	).
+
+run :-
+	/* Simples ja_nein/2 */
+	ja_nein('Bist du schwul? ', Antwort),
+	write(Antwort), nl,
+	
+	/* Aufruf des Praedikats interview/0 */
+	interview.
+
+
+
+
+
+
+
+
+
+
+
