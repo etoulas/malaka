c1c770087ea1837a09cc14525a9b67aacb1a7ab5
efthimios.toulas <efthimios.toulas@d14053f0-d41e-0410-8e62-47ffcaada4dd>
2007-11-26 10:42:43 +0000

    Erster Prolog Code

---
+++ trunk/prolog/befehle.pl
@@ -0,0 +1,12 @@
+emacs.
+prolog_ide(open_navigator).
+prolog_ide(open_debug_status).
+prolog_ide(open_query_window).
+prolog_ide(thread_monitor).
+
+/*
+library(debug).
+prolog_ide(debug_monitor).
+prolog_ide(xref).
+*/
+
