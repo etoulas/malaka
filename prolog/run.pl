6b4b379f47934e9da4885ed0222bbf71f3b4d2d3
m.fanderl <m.fanderl@d14053f0-d41e-0410-8e62-47ffcaada4dd>
2007-12-12 21:15:11 +0000

    fuck you

--- prolog/run.pl
+++ prolog/run.pl
@@ -1,51 +1,69 @@
+/* Teamsport1, Einzelsport2, Partner3, benötigt4, Ausdauer5, Kraft6, Ballsport7, Verletzungsgef8., Zeitfakt9., Koordination10, Sommer11, Winter12, Outdoor13, Indoor14, Zuhause15, Finanzaufw1x16, Finanz.Contin.17, Familientaugl.18, Sozialer Asp.19, Präzision20, Wettkampfbereitsch.21, Schwimmen22 */
+merkmale('Radfahren',1,2,1,3,2,1,2,2,1,2,1,2,1,1,3,1,3,2,1,2,1).
+merkmale('Bowling/Kegeln',2,2,1,1,1,2,1,1,2,2,2,1,2,1,2,2,3,3,3,3,1).
+merkmale('Tauchen',1,1,2,1,1,1,2,2,2,2,1,2,2,1,4,3,3,2,1,1,2).
+merkmale('Golf',1,2,1,1,1,1,1,2,2,2,1,2,1,1,4,4,3,4,3,3,1).
+merkmale('Schwimmen',1,2,1,2,1,1,1,2,2,2,2,2,2,1,1,2,3,2,1,2,2).
+merkmale('Tischtennis',2,1,2,2,1,2,1,2,2,2,2,2,2,2,2,2,3,3,4,3,1).
+merkmale('Eisstockschießen',2,1,1,2,1,1,1,1,2,1,2,2,1,1,2,1,3,4,2,2,1).
+merkmale('Gleitschirm/Fallschirm/Drachenfliegen',1,2,1,2,2,1,3,3,2,2,1,2,1,1,4,4,1,1,2,1,1).
+merkmale('Segeln',2,1,1,2,3,1,2,3,2,2,2,2,1,1,4,4,3,2,1,2,2).
+merkmale('Faustball',3,1,1,3,1,2,2,4,2,2,2,1,2,1,2,2,1,4,3,3,1).
+merkmale('Rudern',2,1,1,4,3,1,2,3,2,2,2,2,1,1,3,2,2,3,1,3,2).
+merkmale('Tanzen',1,1,2,2,1,1,1,1,3,2,2,1,2,2,2,3,2,4,1,2,1).
+merkmale('Yoga',1,2,1,2,2,1,1,2,3,2,2,2,2,2,1,2,3,2,1,1,1).
+merkmale('Schlittschufahren',1,2,1,2,2,1,2,1,3,1,2,2,2,1,2,2,3,2,1,1,1).
+merkmale('Taekwondo/Aikido',1,1,2,2,2,1,2,2,3,2,2,1,2,1,2,2,2,2,2,3,1).
+merkmale('Volleyball',3,1,2,2,2,2,2,4,3,2,2,2,2,1,2,2,2,4,3,3,1).
+merkmale('Windsurfen',1,2,1,2,2,1,2,3,3,2,1,2,1,1,3,2,2,1,1,1,2).
+merkmale('Aerobic',1,2,1,3,1,1,1,2,3,2,2,2,2,2,1,2,1,2,1,1,1).
+merkmale('Badminton',2,1,2,3,1,2,2,2,3,2,2,2,2,1,2,2,2,3,3,3,1).
+merkmale('Inline',1,2,1,3,2,1,3,2,3,2,1,2,2,1,2,1,3,2,1,1,1).
+merkmale('Tennis',2,1,2,3,2,2,2,2,3,2,2,2,2,1,2,2,2,3,3,3,1).
+merkmale('Fussball',3,1,1,3,2,2,3,4,3,2,2,2,2,1,2,2,1,4,3,3,1).
+merkmale('Streethockey',3,1,1,3,2,2,3,2,3,2,2,2,2,1,3,2,2,3,3,3,1).
+merkmale('Judo/Ju-Jutsu/Karate',1,1,2,3,3,1,3,2,3,2,2,1,2,2,2,2,2,2,1,3,1).
+merkmale('Squash',1,1,2,3,3,2,3,2,3,2,2,1,2,1,2,2,1,2,3,3,1).
+merkmale('Rugby',3,1,2,3,3,2,4,2,3,2,2,2,1,1,2,2,1,3,3,3,1).
+merkmale('Klettern',1,2,2,3,4,1,3,3,3,2,2,2,2,1,2,3,3,2,1,1,1).
+merkmale('Boxen',1,1,2,4,4,1,4,4,3,2,2,1,2,1,2,2,1,2,2,4,1).
+merkmale('Wasserball',3,1,2,4,3,2,2,2,3,2,2,2,2,1,1,2,1,4,3,3,2).
+merkmale('Tai Ji',1,2,1,2,1,1,1,2,4,2,2,2,2,2,2,2,1,2,1,1,1).
+merkmale('Skateboard',1,2,1,2,1,1,3,2,4,2,1,2,2,1,2,1,1,4,1,2,1).
+merkmale('Turnen',1,2,1,2,2,1,3,2,4,2,2,1,2,1,2,2,2,2,3,2,1).
+merkmale('Ballet',1,2,2,3,2,1,1,4,4,2,2,1,2,1,1,4,1,2,3,1,1).
+merkmale('Leichtathletik',1,2,1,3,2,1,2,2,4,2,2,2,2,1,2,2,2,2,2,3,1).
+merkmale('Basketball',3,1,2,3,2,2,3,4,4,2,2,2,2,2,2,2,2,4,4,3,1).
+merkmale('Icehockey',3,1,2,3,2,2,3,4,4,1,2,2,2,1,3,2,2,3,4,3,1).
+merkmale('Handball',3,1,2,3,3,2,3,4,4,2,2,1,2,1,2,2,1,4,3,3,1).
+
 /* gewichtung('Teamsport', [0.5, 1, 1.5]). */
 
-/* Teamsport, Einzelsport, Partner, benötigt, Ausdauer, Kraft, Ballsport, Verletzungsgef., Zeitfakt., Koordination, Sommer, Winter, Outdoor, Indoor, Zuhause, Finanzaufw., 1x, Finanz., Contin., Familientaugl., Sozialer, Asp., Präzision, Wettkampfbereitsch., Schwimmen */
-merkmale("Radfahren",1,2,1,3,2,1,2,2,1,2,1,2,1,1,3,1,3,2,1,2,1).
-merkmale("Bowling/Kegeln",2,2,1,1,1,2,1,1,2,2,2,1,2,1,2,2,3,3,3,3,1).
-merkmale("Tauchen",1,1,2,1,1,1,2,2,2,2,1,2,2,1,4,3,3,2,1,1,2).
-merkmale("Golf",1,2,1,1,1,1,1,2,2,2,1,2,1,1,4,4,3,4,3,3,1).
-merkmale("Schwimmen",1,2,1,2,1,1,1,2,2,2,2,2,2,1,1,2,3,2,1,2,2).
-merkmale("Tischtennis",2,1,2,2,1,2,1,2,2,2,2,2,2,2,2,2,3,3,4,3,1).
-merkmale("Eisstockschießen",2,1,1,2,1,1,1,1,2,1,2,2,1,1,2,1,3,4,2,2,1).
-merkmale("Gleitschirm/Fallschirm/Drachenfliegen",1,2,1,2,2,1,3,3,2,2,1,2,1,1,4,4,1,1,2,1,1).
-merkmale("Segeln",2,1,1,2,3,1,2,3,2,2,2,2,1,1,4,4,3,2,1,2,2).
-merkmale("Faustball",3,1,1,3,1,2,2,4,2,2,2,1,2,1,2,2,1,4,3,3,1).
-merkmale("Rudern",2,1,1,4,3,1,2,3,2,2,2,2,1,1,3,2,2,3,1,3,2).
-merkmale("Tanzen",1,1,2,2,1,1,1,1,3,2,2,1,2,2,2,3,2,4,1,2,1).
-merkmale("Yoga",1,2,1,2,2,1,1,2,3,2,2,2,2,2,1,2,3,2,1,1,1).
-merkmale("Schlittschufahren",1,2,1,2,2,1,2,1,3,1,2,2,2,1,2,2,3,2,1,1,1).
-merkmale("Taekwondo/Aikido",1,1,2,2,2,1,2,2,3,2,2,1,2,1,2,2,2,2,2,3,1).
-merkmale("Volleyball",3,1,2,2,2,2,2,4,3,2,2,2,2,1,2,2,2,4,3,3,1).
-merkmale("Windsurfen",1,2,1,2,2,1,2,3,3,2,1,2,1,1,3,2,2,1,1,1,2).
-merkmale("Aerobic",1,2,1,3,1,1,1,2,3,2,2,2,2,2,1,2,1,2,1,1,1).
-merkmale("Badminton",2,1,2,3,1,2,2,2,3,2,2,2,2,1,2,2,2,3,3,3,1).
-merkmale("Inline",1,2,1,3,2,1,3,2,3,2,1,2,2,1,2,1,3,2,1,1,1).
-merkmale("Tennis",2,1,2,3,2,2,2,2,3,2,2,2,2,1,2,2,2,3,3,3,1).
-merkmale("Fussball",3,1,1,3,2,2,3,4,3,2,2,2,2,1,2,2,1,4,3,3,1).
-merkmale("Streethockey",3,1,1,3,2,2,3,2,3,2,2,2,2,1,3,2,2,3,3,3,1).
-merkmale("Judo/Ju-Jutsu/Karate",1,1,2,3,3,1,3,2,3,2,2,1,2,2,2,2,2,2,1,3,1).
-merkmale("Squash",1,1,2,3,3,2,3,2,3,2,2,1,2,1,2,2,1,2,3,3,1).
-merkmale("Rugby",3,1,2,3,3,2,4,2,3,2,2,2,1,1,2,2,1,3,3,3,1).
-merkmale("Klettern",1,2,2,3,4,1,3,3,3,2,2,2,2,1,2,3,3,2,1,1,1).
-merkmale("Boxen",1,1,2,4,4,1,4,4,3,2,2,1,2,1,2,2,1,2,2,4,1).
-merkmale("Wasserball",3,1,2,4,3,2,2,2,3,2,2,2,2,1,1,2,1,4,3,3,2).
-merkmale("Tai Ji",1,2,1,2,1,1,1,2,4,2,2,2,2,2,2,2,1,2,1,1,1).
-merkmale("Skateboard",1,2,1,2,1,1,3,2,4,2,1,2,2,1,2,1,1,4,1,2,1).
-merkmale("Turnen",1,2,1,2,2,1,3,2,4,2,2,1,2,1,2,2,2,2,3,2,1).
-merkmale("Ballet",1,2,2,3,2,1,1,4,4,2,2,1,2,1,1,4,1,2,3,1,1).
-merkmale("Leichtathletik",1,2,1,3,2,1,2,2,4,2,2,2,2,1,2,2,2,2,2,3,1).
-merkmale("Basketball",3,1,2,3,2,2,3,4,4,2,2,2,2,2,2,2,2,4,4,3,1).
-merkmale("Icehockey",3,1,2,3,2,2,3,4,4,1,2,2,2,1,3,2,2,3,4,3,1).
-merkmale("Handball",3,1,2,3,3,2,3,4,4,2,2,1,2,1,2,2,1,4,3,3,1).
+frageko(schwimmen,   'Wollen Sie einen Wassersport betreiben?').
+frageko(einzelsport, 'Moechten Sie alleine Sport machen?').
+frageko(partnersport,'Stoert es Sie auf einen Partner angewiesen zu sein?').
+frageko(teamsport,   'Moechten Sie in einem Team Sport machen?').
+frageko(ballsport,   'Moegen Sie Ballsport?').
+frageko(sommersport, 'Moechten Sie im Sommer Sport treiben?').
+frageko(wintersport, 'Moechten Sie im Winter Sport treiben?').
+frageko(outdoor,     'Moechten Sie im Freien Sport treiben?').
+frageko(indoor,      'Moechten Sie in einem Gebaeude Sport betreiben?').
+frageko(zuhause,     'Moechten Sie zuhause Sport betreiben?').
 
 /* writeseq/1 gibt eine Liste in einer Zeile aus */
 writeseq([]).
 writeseq([A|B]) :- write(A), writeseq(B).
 
-ja_nein(Anfrage, Antwort) :-
+/* Element hinten anfuegen
+ * hintenran(Liste, Elem, Erg).
+ */
+hintenran([],Elem,[Elem]). % Element als Liste ins Ergebnis kopieren
+hintenran([Kopf|Rest],Elem,[Kopf|Ergebnis]) :-
+	hintenran(Rest,Elem,Ergebnis),!.
+
+ja_nein(Anfrage,Antwort) :-
 	repeat,
-	    write('(j/n) Antworten Sie mit "j." f�r JA oder mit "n." f�r NEIN: '), nl,
+	    write('Antworten Sie mit "j." fuer JA oder mit "n." fuer NEIN: '), nl,
 	    writeseq([Anfrage, ' ']),
 	    read(L),
 	    (	L == j, Antwort = ja, !
@@ -54,31 +72,42 @@ ja_nein(Anfrage, Antwort) :-
 		fail
 	    ).
 
-frage(schwimmen, 'K�nnen Sie schwimmen?').
+:- dynamic kokriterien/1.
+kokriterien([]).
+
+merkeko(X) :-
+	kokriterien(K),
+	hintenran(K, X, Kneu),
+	retract(kokriterien(K)),
+	assert(kokriterien(Kneu)),
+	!.
 
 interview :-
-	frage(Context,Anfrage),
-	ja_nein(Anfrage, Antwort),
+	frageko(Context,Anfrage),
+	ja_nein(Anfrage,Antwort),
 	(   Antwort == ja,
-	    nl, writeseq([Context, ': ', Antwort])
+	    merkeko(2),
+	    writeseq(['DEBUG: ',Context]),nl,
+	    fail
 	 ;  fail
 	).
 
 run :-
 	/* Simples ja_nein/2 */
-	ja_nein('Bist du schwul? ', Antwort),
+	/* ja_nein('Bist du schwul? ', Antwort),
 	write(Antwort), nl,
 	
+	
+	merkmale(A,B,C,D,E,F,G,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_),
+	write(A), write(' '),
+	writeseq([B,C,D,E,F,G]),nl,
+	*/
 	/* Aufruf des Praedikats interview/0 */
-	interview.
-
-
-
-
-
-
-
-
+	kokriterien(A),
+	retract(kokriterien(A)),
+	assert(kokriterien([])),
+	interview,
+	kokriterien(K),writeseq(K),nl.
 
 
 
