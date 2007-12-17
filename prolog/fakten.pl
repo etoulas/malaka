15e035ff49a7efc68e4b6a35582a72fa31308d62
efthimios.toulas <etoulas@users.noreply.github.com>
2007-12-17 16:16:22 +0000

    fakten.pl hinzugefügt

---
+++ prolog/fakten.pl
@@ -0,0 +1,61 @@
+/*
+KO:
+'Sportarten','Schwimmen','Einzelsport','Partner benoetigt','Teamsport','Ballsport','Sommer','Winter','Outdoor','Indoor','Zuhause',
+Rest:
+'Ausdauer','Kraft','Verletzungsgef.','Zeitfakt.','Koordination','Finanzaufw. 1x','Finanz. Contin.','Familientaugl.','Sozialer Asp.','Präzision','Wettkampfbereitsch.'
+*/
+merkmale(['Schach',1,1,2,1,1,2,2,2,2,2,1,1,1,1,1,1,2,3,2,1,4]).
+merkmale(['Darten',1,2,1,1,1,2,2,1,2,2,1,1,1,1,1,1,2,3,3,4,3]).
+merkmale(['Schiessen',1,2,1,1,1,2,2,2,2,2,1,1,1,1,1,2,2,3,3,4,2]).
+merkmale(['Billard',1,1,1,2,1,2,2,1,2,2,1,1,1,1,1,2,2,3,3,4,3]).
+merkmale(['Reiten',1,2,1,1,1,2,2,2,2,1,2,2,3,2,1,3,4,3,2,1,1]).
+merkmale(['Laufen',1,2,1,1,1,2,2,2,2,2,3,2,2,2,1,2,1,3,1,1,2]).
+merkmale(['Radfahren',1,2,1,1,1,2,1,2,1,1,3,2,2,2,1,3,1,3,2,1,2]).
+merkmale(['Bowling/Kegeln',1,2,1,2,2,2,2,1,2,1,1,1,1,1,2,2,2,3,3,3,3]).
+merkmale(['Tauchen',2,1,2,1,1,2,1,2,2,1,1,1,2,2,2,4,3,3,2,1,1]).
+merkmale(['Golf',1,2,1,1,1,2,1,2,1,1,1,1,1,2,2,4,4,3,4,3,3]).
+merkmale(['Schwimmen',2,2,1,1,1,2,2,2,2,1,2,1,1,2,2,1,2,3,2,1,2]).
+merkmale(['Tischtennis',1,1,2,2,2,2,2,2,2,2,2,1,1,2,2,2,2,3,3,4,3]).
+merkmale(['Eisstockschiessen',1,1,1,2,1,1,2,2,1,1,2,1,1,1,2,2,1,3,4,2,2]).
+merkmale(['Gleitschirm/Fallschirm/Drachenfliegen',1,2,1,1,1,2,1,2,1,1,2,2,3,3,2,4,4,1,1,2,1]).
+merkmale(['Segeln',2,1,1,2,1,2,2,2,1,1,2,3,2,3,2,4,4,3,2,1,2]).
+merkmale(['Faustball',1,1,1,3,2,2,2,1,2,1,3,1,2,4,2,2,2,1,4,3,3]).
+merkmale(['Rudern',2,1,1,2,1,2,2,2,1,1,4,3,2,3,2,3,2,2,3,1,3]).
+merkmale(['Tanzen',1,1,2,1,1,2,2,1,2,2,2,1,1,1,3,2,3,2,4,1,2]).
+merkmale(['Yoga',1,2,1,1,1,2,2,2,2,2,2,2,1,2,3,1,2,3,2,1,1]).
+merkmale(['Schlittschufahren',1,2,1,1,1,1,2,2,2,1,2,2,2,1,3,2,2,3,2,1,1]).
+merkmale(['Taekwondo/Aikido',1,1,2,1,1,2,2,1,2,1,2,2,2,2,3,2,2,2,2,2,3]).
+merkmale(['Volleyball',1,1,2,3,2,2,2,2,2,1,2,2,2,4,3,2,2,2,4,3,3]).
+merkmale(['Windsurfen',2,2,1,1,1,2,1,2,1,1,2,2,2,3,3,3,2,2,1,1,1]).
+merkmale(['Aerobic',1,2,1,1,1,2,2,2,2,2,3,1,1,2,3,1,2,1,2,1,1]).
+merkmale(['Badminton',1,1,2,2,2,2,2,2,2,1,3,1,2,2,3,2,2,2,3,3,3]).
+merkmale(['Inline',1,2,1,1,1,2,1,2,2,1,3,2,3,2,3,2,1,3,2,1,1]).
+merkmale(['Tennis',1,1,2,2,2,2,2,2,2,1,3,2,2,2,3,2,2,2,3,3,3]).
+merkmale(['Fussball',1,1,1,3,2,2,2,2,2,1,3,2,3,4,3,2,2,1,4,3,3]).
+merkmale(['Streethockey',1,1,1,3,2,2,2,2,2,1,3,2,3,2,3,3,2,2,3,3,3]).
+merkmale(['Judo/Ju-Jutsu/Karate',1,1,2,1,1,2,2,1,2,2,3,3,3,2,3,2,2,2,2,1,3]).
+merkmale(['Squash',1,1,2,1,2,2,2,1,2,1,3,3,3,2,3,2,2,1,2,3,3]).
+merkmale(['Rugby',1,1,2,3,2,2,2,2,1,1,3,3,4,2,3,2,2,1,3,3,3]).
+merkmale(['Klettern',1,2,2,1,1,2,2,2,2,1,3,4,3,3,3,2,3,3,2,1,1]).
+merkmale(['Boxen',1,1,2,1,1,2,2,1,2,1,4,4,4,4,3,2,2,1,2,2,4]).
+merkmale(['Wasserball',2,1,2,3,2,2,2,2,2,1,4,3,2,2,3,1,2,1,4,3,3]).
+merkmale(['Tai Ji',1,2,1,1,1,2,2,2,2,2,2,1,1,2,4,2,2,1,2,1,1]).
+merkmale(['Skateboard',1,2,1,1,1,2,1,2,2,1,2,1,3,2,4,2,1,1,4,1,2]).
+merkmale(['Turnen',1,2,1,1,1,2,2,1,2,1,2,2,3,2,4,2,2,2,2,3,2]).
+merkmale(['Ballet',1,2,2,1,1,2,2,1,2,1,3,2,1,4,4,1,4,1,2,3,1]).
+merkmale(['Leichtathletik',1,2,1,1,1,2,2,2,2,1,3,2,2,2,4,2,2,2,2,2,3]).
+merkmale(['Basketball',1,1,2,3,2,2,2,2,2,2,3,2,3,4,4,2,2,2,4,4,3]).
+merkmale(['Icehockey',1,1,2,3,2,1,2,2,2,1,3,2,3,4,4,3,2,2,3,4,3]).
+merkmale(['Handball',1,1,2,3,2,2,2,1,2,1,3,3,3,4,4,2,2,1,4,3,3]).
+
+/* 10 KO-Fragen */
+frageko(schwimmen,   'Wollen Sie einen Wassersport betreiben?').
+frageko(einzelsport, 'Moechten Sie alleine Sport machen?').
+frageko(partnersport,'Stoert es Sie auf einen Partner angewiesen zu sein?').
+frageko(teamsport,   'Moechten Sie in einem Team Sport machen?').
+frageko(ballsport,   'Moegen Sie Ballsport?').
+frageko(sommersport, 'Moechten Sie im Sommer Sport treiben?').
+frageko(wintersport, 'Moechten Sie im Winter Sport treiben?').
+frageko(outdoor,     'Moechten Sie im Freien Sport treiben?').
+frageko(indoor,      'Moechten Sie in einem Gebaeude Sport betreiben?').
+frageko(zuhause,     'Moechten Sie zuhause Sport betreiben?').
