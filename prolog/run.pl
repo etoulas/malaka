2298cd820e0393999d6897628badbfc5051eb2e2
efthimios.toulas <etoulas@users.noreply.github.com>
2007-12-17 16:13:34 +0000

    Latest and greatest...

--- prolog/run.pl
+++ prolog/run.pl
@@ -1,70 +1,7 @@
-/*
-KO:
-'Sportarten','Schwimmen','Einzelsport','Partner benoetigt','Teamsport','Ballsport','Sommer','Winter','Outdoor','Indoor','Zuhause',
-Rest:
-'Ausdauer','Kraft','Verletzungsgef.','Zeitfakt.','Koordination','Finanzaufw. 1x','Finanz. Contin.','Familientaugl.','Sozialer Asp.','Präzision','Wettkampfbereitsch.'
-*/
-merkmale('Schach',1,1,2,1,1,2,2,2,2,2,1,1,1,1,1,1,2,3,2,1,4,34).
-merkmale('Darten',1,2,1,1,1,2,2,1,2,2,1,1,1,1,1,1,2,3,3,4,3,36).
-merkmale('Schiessen',1,2,1,1,1,2,2,2,2,2,1,1,1,1,1,2,2,3,3,4,2,37).
-merkmale('Billard',1,1,1,2,1,2,2,1,2,2,1,1,1,1,1,2,2,3,3,4,3,37).
-merkmale('Reiten',1,2,1,1,1,2,2,2,2,1,2,2,3,2,1,3,4,3,2,1,1,39).
-merkmale('Laufen',1,2,1,1,1,2,2,2,2,2,3,2,2,2,1,2,1,3,1,1,2,36).
-merkmale('Radfahren',1,2,1,1,1,2,1,2,1,1,3,2,2,2,1,3,1,3,2,1,2,35).
-merkmale('Bowling/Kegeln',1,2,1,2,2,2,2,1,2,1,1,1,1,1,2,2,2,3,3,3,3,38).
-merkmale('Tauchen',2,1,2,1,1,2,1,2,2,1,1,1,2,2,2,4,3,3,2,1,1,37).
-merkmale('Golf',1,2,1,1,1,2,1,2,1,1,1,1,1,2,2,4,4,3,4,3,3,41).
-merkmale('Schwimmen',2,2,1,1,1,2,2,2,2,1,2,1,1,2,2,1,2,3,2,1,2,35).
-merkmale('Tischtennis',1,1,2,2,2,2,2,2,2,2,2,1,1,2,2,2,2,3,3,4,3,43).
-merkmale('Eisstockschiessen',1,1,1,2,1,1,2,2,1,1,2,1,1,1,2,2,1,3,4,2,2,34).
-merkmale('Gleitschirm/Fallschirm/Drachenfliegen',1,2,1,1,1,2,1,2,1,1,2,2,3,3,2,4,4,1,1,2,1,38).
-merkmale('Segeln',2,1,1,2,1,2,2,2,1,1,2,3,2,3,2,4,4,3,2,1,2,43).
-merkmale('Faustball',1,1,1,3,2,2,2,1,2,1,3,1,2,4,2,2,2,1,4,3,3,43).
-merkmale('Rudern',2,1,1,2,1,2,2,2,1,1,4,3,2,3,2,3,2,2,3,1,3,43).
-merkmale('Tanzen',1,1,2,1,1,2,2,1,2,2,2,1,1,1,3,2,3,2,4,1,2,37).
-merkmale('Yoga',1,2,1,1,1,2,2,2,2,2,2,2,1,2,3,1,2,3,2,1,1,36).
-merkmale('Schlittschufahren',1,2,1,1,1,1,2,2,2,1,2,2,2,1,3,2,2,3,2,1,1,35).
-merkmale('Taekwondo/Aikido',1,1,2,1,1,2,2,1,2,1,2,2,2,2,3,2,2,2,2,2,3,38).
-merkmale('Volleyball',1,1,2,3,2,2,2,2,2,1,2,2,2,4,3,2,2,2,4,3,3,47).
-merkmale('Windsurfen',2,2,1,1,1,2,1,2,1,1,2,2,2,3,3,3,2,2,1,1,1,36).
-merkmale('Aerobic',1,2,1,1,1,2,2,2,2,2,3,1,1,2,3,1,2,1,2,1,1,34).
-merkmale('Badminton',1,1,2,2,2,2,2,2,2,1,3,1,2,2,3,2,2,2,3,3,3,43).
-merkmale('Inline',1,2,1,1,1,2,1,2,2,1,3,2,3,2,3,2,1,3,2,1,1,37).
-merkmale('Tennis',1,1,2,2,2,2,2,2,2,1,3,2,2,2,3,2,2,2,3,3,3,44).
-merkmale('Fussball',1,1,1,3,2,2,2,2,2,1,3,2,3,4,3,2,2,1,4,3,3,47).
-merkmale('Streethockey',1,1,1,3,2,2,2,2,2,1,3,2,3,2,3,3,2,2,3,3,3,46).
-merkmale('Judo/Ju-Jutsu/Karate',1,1,2,1,1,2,2,1,2,2,3,3,3,2,3,2,2,2,2,1,3,41).
-merkmale('Squash',1,1,2,1,2,2,2,1,2,1,3,3,3,2,3,2,2,1,2,3,3,42).
-merkmale('Rugby',1,1,2,3,2,2,2,2,1,1,3,3,4,2,3,2,2,1,3,3,3,46).
-merkmale('Klettern',1,2,2,1,1,2,2,2,2,1,3,4,3,3,3,2,3,3,2,1,1,44).
-merkmale('Boxen',1,1,2,1,1,2,2,1,2,1,4,4,4,4,3,2,2,1,2,2,4,46).
-merkmale('Wasserball',2,1,2,3,2,2,2,2,2,1,4,3,2,2,3,1,2,1,4,3,3,47).
-merkmale('Tai Ji',1,2,1,1,1,2,2,2,2,2,2,1,1,2,4,2,2,1,2,1,1,35).
-merkmale('Skateboard',1,2,1,1,1,2,1,2,2,1,2,1,3,2,4,2,1,1,4,1,2,37).
-merkmale('Turnen',1,2,1,1,1,2,2,1,2,1,2,2,3,2,4,2,2,2,2,3,2,40).
-merkmale('Ballet',1,2,2,1,1,2,2,1,2,1,3,2,1,4,4,1,4,1,2,3,1,41).
-merkmale('Leichtathletik',1,2,1,1,1,2,2,2,2,1,3,2,2,2,4,2,2,2,2,2,3,41).
-merkmale('Basketball',1,1,2,3,2,2,2,2,2,2,3,2,3,4,4,2,2,2,4,4,3,52).
-merkmale('Icehockey',1,1,2,3,2,1,2,2,2,1,3,2,3,4,4,3,2,2,3,4,3,50).
-merkmale('Handball',1,1,2,3,2,2,2,1,2,1,3,3,3,4,4,2,2,1,4,3,3,49).
-
-/* gewichtung('Teamsport', [0.5, 1, 1.5]). */
-
-/* 10 KO-Fragen */
-frageko(schwimmen,   'Wollen Sie einen Wassersport betreiben?').
-frageko(einzelsport, 'Moechten Sie alleine Sport machen?').
-frageko(partnersport,'Stoert es Sie auf einen Partner angewiesen zu sein?').
-frageko(teamsport,   'Moechten Sie in einem Team Sport machen?').
-frageko(ballsport,   'Moegen Sie Ballsport?').
-frageko(sommersport, 'Moechten Sie im Sommer Sport treiben?').
-frageko(wintersport, 'Moechten Sie im Winter Sport treiben?').
-frageko(outdoor,     'Moechten Sie im Freien Sport treiben?').
-frageko(indoor,      'Moechten Sie in einem Gebaeude Sport betreiben?').
-frageko(zuhause,     'Moechten Sie zuhause Sport betreiben?').
-
 /* writeseq/1 gibt eine Liste in einer Zeile aus */
 writeseq([]).
 writeseq([A|B]) :- write(A), writeseq(B).
+
 
 /* Element hinten anfuegen
  * hintenran(Liste, Elem, Erg).
@@ -72,6 +9,17 @@ writeseq([A|B]) :- write(A), writeseq(B).
 hintenran([],Elem,[Elem]). % Element als Liste ins Ergebnis kopieren
 hintenran([Kopf|Rest],Elem,[Kopf|Ergebnis]) :-
 	hintenran(Rest,Elem,Ergebnis),!.
+
+
+/* ja_nein/2 fuer Teamsport */
+ja_nein(Anfrage,Antwort) :-
+	Anfrage == 'Moechten Sie in einem Team Sport machen?',
+	/* write('Sport ja/nein'),nl, */
+	    nonvar(jaeinzelflag),
+	    nonvar(japartnerflag),
+	    Antwort = ja, !,
+	    loesche_flags.
+
 
 ja_nein(Anfrage,Antwort) :-
 	repeat,
@@ -82,44 +30,86 @@ ja_nein(Anfrage,Antwort) :-
 	     ;	L == n, Antwort = nein, !
 	     ;	nl, write('Falsche Eingabe.'), nl,
 		fail
+	    
 	    ).
 
-:- dynamic kokriterien/1.
-:-
-	/* Liste mit KO Antworten zuruecksetzen */
-	kokriterien(A),
-	retract(kokriterien(A)),
-	assert(kokriterien([]))
-	;
-	true.
 
-merkeko(X) :-
+:- dynamic kokriterien/1.
+
+/* alle KO Antworten in kokriterien/2 loeschen */
+loesche_kokriterien :- retract(kokriterien(C,A)),fail.
+loesche_kokriterien.
+
+merkeko(Context, Antwort) :- assertz(kokriterien(Context,Antwort)).
+
+/* Liste mit KO Antworten zuruecksetzen */
+loesche_kokriterien_liste :- retract(kokriterien(A)),fail.
+loesche_kokriterien_liste.
+
+/* Speichern in Liste */
+merkeko_in_liste(X) :-
 	kokriterien(K),
 	hintenran(K, X, Kneu),
 	retract(kokriterien(K)),
 	assert(kokriterien(Kneu)),
 	!.
 
+
+:- dynamic jaeinzelflag/0.
+
+loesche_jaeinzelflag :- retract(jaeinzelflag(1)),fail.
+loesche_jaeinzelflag.
+
+set_jaeinzelflag :- assert(jaeinzelflag(1)),!.
+
+
+:- dynamic japartnerflag/0.
+
+loesche_japartnerflag :- retract(japartnerflag(1)),fail.
+loesche_japartnerflag.
+
+set_japartnerflag :- assert(japartnerflag(1)),!.
+
+
+/* japartnerflag und jaeinzelflag zuruecksetzen */
+loesche_flags :- loesche_jaeinzelflag,loesche_japartnerflag.
+
+
+/* KO Fragen abfragen */
 interview :-
 	frageko(Context,Anfrage),
 	ja_nein(Anfrage,Antwort),
 	(   Antwort == ja,
-	    merkeko(2),
-	    writeseq(['DEBUG: ',Context]),nl
+	    merkeko(Context,2),
+	    Context == partnersport, set_japartnerflag    
 	;
 	    Antwort == nein,
-	    merkeko(1),
-	    write('nein'),nl
+	    merkeko(Context,1),
+	    Context == einzelsport, set_jaeinzelflag	
 	),
 	fail.
+interview.
+
+/* Ausgeben aller KO Antworten in kokriterien/2 */
+zeige_ko :- kokriterien(C,K),writeseq([C,': ',K]),nl,fail.
+zeige_ko.
 
 run :-
-	assert(kokriterien([])),
+	loesche_kokriterien,
+	asserta(kokriterien([])),
 	/* Aufruf des Praedikats interview/0 */
 	interview,
-	
-	/* Ausgeben der Liste mit KO Antworten */
-	kokriterien(K),writeseq(K),nl.
+	zeige_ko.
+run.
 
-test :-
-	assert(kokriterien([1, 1, 1, 2, 2, 2, 2, 2, 2, 1])).
+
+
+/*
+ * Ausfiltern der Sportarten nach KO-Antworten
+ * kofilter(Kriterien,Merkmale,Ergebnis).
+ */
+/*
+kofilter([],_,_).
+kofilter([Kkrit|Rkrit],[Kmerk|Rmerk],[Kerg|Rerg]) :-
+	kofilter(Rkrit,Rmerk,Rerg).
+*/
