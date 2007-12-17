15afd0632c5b8dcb35f52f7d2fdf5ae157769c54
efthimios.toulas <etoulas@users.noreply.github.com>
2007-12-17 14:48:27 +0000

    Tinos Zwischenstand

---
+++ prolog/run3.pl
@@ -0,0 +1,176 @@
+/*
+KO:
+'Sportarten','Schwimmen','Einzelsport','Partner benoetigt','Teamsport','Ballsport','Sommer','Winter','Outdoor','Indoor','Zuhause',
+Rest:
+'Ausdauer','Kraft','Verletzungsgef.','Zeitfakt.','Koordination','Finanzaufw. 1x','Finanz. Contin.','Familientaugl.','Sozialer Asp.','Präzision','Wettkampfbereitsch.'
+*/
+merkmale(['Schach',1,1,2,1,1,2,2,2,2,2,1,1,1,1,1,1,2,3,2,1,4]).
+merkmale(['Darten',1,2,1,1,1,2,2,1,2,2,1,1,1,1,1,1,2,3,3,4,3]).
+merkmale(['Schiessen',1,2,1,1,1,2,2,2,2,2,1,1,1,1,1,2,2,3,3,4,2]).
+merkmale(['Billard',1,1,1,2,1,2,2,1,2,2,1,1,1,1,1,2,2,3,3,4,3]).
+merkmale(['Reiten',1,2,1,1,1,2,2,2,2,1,2,2,3,2,1,3,4,3,2,1,1]).
+merkmale(['Laufen',1,2,1,1,1,2,2,2,2,2,3,2,2,2,1,2,1,3,1,1,2]).
+merkmale(['Radfahren',1,2,1,1,1,2,1,2,1,1,3,2,2,2,1,3,1,3,2,1,2]).
+merkmale(['Bowling/Kegeln',1,2,1,2,2,2,2,1,2,1,1,1,1,1,2,2,2,3,3,3,3]).
+merkmale(['Tauchen',2,1,2,1,1,2,1,2,2,1,1,1,2,2,2,4,3,3,2,1,1]).
+merkmale(['Golf',1,2,1,1,1,2,1,2,1,1,1,1,1,2,2,4,4,3,4,3,3]).
+merkmale(['Schwimmen',2,2,1,1,1,2,2,2,2,1,2,1,1,2,2,1,2,3,2,1,2]).
+merkmale(['Tischtennis',1,1,2,2,2,2,2,2,2,2,2,1,1,2,2,2,2,3,3,4,3]).
+merkmale(['Eisstockschiessen',1,1,1,2,1,1,2,2,1,1,2,1,1,1,2,2,1,3,4,2,2]).
+merkmale(['Gleitschirm/Fallschirm/Drachenfliegen',1,2,1,1,1,2,1,2,1,1,2,2,3,3,2,4,4,1,1,2,1]).
+merkmale(['Segeln',2,1,1,2,1,2,2,2,1,1,2,3,2,3,2,4,4,3,2,1,2]).
+merkmale(['Faustball',1,1,1,3,2,2,2,1,2,1,3,1,2,4,2,2,2,1,4,3,3]).
+merkmale(['Rudern',2,1,1,2,1,2,2,2,1,1,4,3,2,3,2,3,2,2,3,1,3]).
+merkmale(['Tanzen',1,1,2,1,1,2,2,1,2,2,2,1,1,1,3,2,3,2,4,1,2]).
+merkmale(['Yoga',1,2,1,1,1,2,2,2,2,2,2,2,1,2,3,1,2,3,2,1,1]).
+merkmale(['Schlittschufahren',1,2,1,1,1,1,2,2,2,1,2,2,2,1,3,2,2,3,2,1,1]).
+merkmale(['Taekwondo/Aikido',1,1,2,1,1,2,2,1,2,1,2,2,2,2,3,2,2,2,2,2,3]).
+merkmale(['Volleyball',1,1,2,3,2,2,2,2,2,1,2,2,2,4,3,2,2,2,4,3,3]).
+merkmale(['Windsurfen',2,2,1,1,1,2,1,2,1,1,2,2,2,3,3,3,2,2,1,1,1]).
+merkmale(['Aerobic',1,2,1,1,1,2,2,2,2,2,3,1,1,2,3,1,2,1,2,1,1]).
+merkmale(['Badminton',1,1,2,2,2,2,2,2,2,1,3,1,2,2,3,2,2,2,3,3,3]).
+merkmale(['Inline',1,2,1,1,1,2,1,2,2,1,3,2,3,2,3,2,1,3,2,1,1]).
+merkmale(['Tennis',1,1,2,2,2,2,2,2,2,1,3,2,2,2,3,2,2,2,3,3,3]).
+merkmale(['Fussball',1,1,1,3,2,2,2,2,2,1,3,2,3,4,3,2,2,1,4,3,3]).
+merkmale(['Streethockey',1,1,1,3,2,2,2,2,2,1,3,2,3,2,3,3,2,2,3,3,3]).
+merkmale(['Judo/Ju-Jutsu/Karate',1,1,2,1,1,2,2,1,2,2,3,3,3,2,3,2,2,2,2,1,3]).
+merkmale(['Squash',1,1,2,1,2,2,2,1,2,1,3,3,3,2,3,2,2,1,2,3,3]).
+merkmale(['Rugby',1,1,2,3,2,2,2,2,1,1,3,3,4,2,3,2,2,1,3,3,3]).
+merkmale(['Klettern',1,2,2,1,1,2,2,2,2,1,3,4,3,3,3,2,3,3,2,1,1]).
+merkmale(['Boxen',1,1,2,1,1,2,2,1,2,1,4,4,4,4,3,2,2,1,2,2,4]).
+merkmale(['Wasserball',2,1,2,3,2,2,2,2,2,1,4,3,2,2,3,1,2,1,4,3,3]).
+merkmale(['Tai Ji',1,2,1,1,1,2,2,2,2,2,2,1,1,2,4,2,2,1,2,1,1]).
+merkmale(['Skateboard',1,2,1,1,1,2,1,2,2,1,2,1,3,2,4,2,1,1,4,1,2]).
+merkmale(['Turnen',1,2,1,1,1,2,2,1,2,1,2,2,3,2,4,2,2,2,2,3,2]).
+merkmale(['Ballet',1,2,2,1,1,2,2,1,2,1,3,2,1,4,4,1,4,1,2,3,1]).
+merkmale(['Leichtathletik',1,2,1,1,1,2,2,2,2,1,3,2,2,2,4,2,2,2,2,2,3]).
+merkmale(['Basketball',1,1,2,3,2,2,2,2,2,2,3,2,3,4,4,2,2,2,4,4,3]).
+merkmale(['Icehockey',1,1,2,3,2,1,2,2,2,1,3,2,3,4,4,3,2,2,3,4,3]).
+merkmale(['Handball',1,1,2,3,2,2,2,1,2,1,3,3,3,4,4,2,2,1,4,3,3]).
+
+/* gewichtung('Teamsport', [0.5, 1, 1.5]). */
+
+/* 10 KO-Fragen */
+frageko(schwimmen,   'Wollen Sie einen Wassersport betreiben?').
+frageko(einzelsport, 'Moechten Sie alleine Sport machen?').
+frageko(partnersport,'Stoert es Sie auf einen Partner angewiesen zu sein?').
+frageko(teamsport,   'Moechten Sie in einem Team Sport machen?').
+frageko(ballsport,   'Moegen Sie Ballsport?').
+frageko(sommersport, 'Moechten Sie im Sommer Sport treiben?').
+frageko(wintersport, 'Moechten Sie im Winter Sport treiben?').
+frageko(outdoor,     'Moechten Sie im Freien Sport treiben?').
+frageko(indoor,      'Moechten Sie in einem Gebaeude Sport betreiben?').
+frageko(zuhause,     'Moechten Sie zuhause Sport betreiben?').
+
+
+
+
+/* writeseq/1 gibt eine Liste in einer Zeile aus */
+writeseq([]).
+writeseq([A|B]) :- write(A), writeseq(B).
+
+
+
+/* Element hinten anfuegen
+ * hintenran(Liste, Elem, Erg).
+ */
+hintenran([],Elem,[Elem]). % Element als Liste ins Ergebnis kopieren
+hintenran([Kopf|Rest],Elem,[Kopf|Ergebnis]) :-
+	hintenran(Rest,Elem,Ergebnis),!.
+
+
+
+
+ja_nein(Anfrage,Antwort) :-
+	Anfrage == 'Moechten Sie in einem Team Sport machen?',
+	    nonvar(jaflag),
+	    Antwort = ja, !,
+	    loesche_jaflag.
+
+
+
+
+ja_nein(Anfrage,Antwort) :-
+	repeat,
+	    /* write('Antworten Sie mit "j." fuer JA oder mit "n." fuer NEIN: '), nl, */
+	    writeseq([Anfrage, ' (j./n.) ']),
+	    read(L),
+	    (	L == j, Antwort = ja, !
+	     ;	L == n, Antwort = nein, !
+	     ;	nl, write('Falsche Eingabe.'), nl,
+		fail
+	    ).
+
+
+:- dynamic kokriterien/1.
+loesche_kokriterien :-
+	/* Liste mit KO Antworten zuruecksetzen */
+	retract(kokriterien(A)),fail.
+loesche_kokriterien.
+
+merkeko(X) :-
+	kokriterien(K),
+	hintenran(K, X, Kneu),
+	retract(kokriterien(K)),
+	assert(kokriterien(Kneu)),
+	!.
+
+
+
+
+:- dynamic jaflag/0.
+loesche_jaflag:-
+	/* Liste mit KO Antworten zuruecksetzen */
+	retract(jaflag(1)),fail.
+loesche_jaflag.
+
+set_jaflag :-
+	assert(jaflag(1)),
+	!.
+
+
+
+interview :-
+	frageko(Context,Anfrage),
+/*	(   Context == teamsport,
+	    jaflag == 1,
+	    merkeko(2),
+	    loesche_jaflag ); */
+	
+	ja_nein(Anfrage,Antwort),
+	(   Antwort == ja,
+	    merkeko(2),
+	    (	
+	    Context == einzelsport, set_jaflag;
+    	    Context == partnersport, set_jaflag	    
+	    )
+	;
+	    Antwort == nein,
+	    merkeko(1)
+	),
+	fail.
+interview.
+
+
+
+run :-
+	loesche_kokriterien,
+	asserta(kokriterien([])),
+	/* Aufruf des Praedikats interview/0 */
+	interview,
+	
+	/* Ausgeben der Liste mit KO Antworten */
+	kokriterien(K),writeseq(K),nl.
+
+
+
+/*
+ * Ausfiltern der Sportarten nach KO-Antworten
+ * kofilter(Kriterien,Merkmale,Ergebnis).
+ */
+
+
+kofilter([],_,_).
+kofilter([Kkrit|Rkrit],[Kmerk|Rmerk],[Kerg|Rerg]) :-
+	kofilter(Rkrit,Rmerk,Rerg)
+	.
