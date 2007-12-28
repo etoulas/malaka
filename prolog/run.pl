8e204e58057b818b6ade02b071237ca30f762539
efthimios.toulas <etoulas@users.noreply.github.com>
2007-12-28 22:31:32 +0000

    Auswertung der KO Fragen

--- prolog/run.pl
+++ prolog/run.pl
@@ -2,6 +2,15 @@
 writeseq([]).
 writeseq([A|B]) :- write(A), writeseq(B).
 
+/* Abfragen des Elements an Position beginnend bei 1 */
+pos_in_liste(Liste,Position,Element) :- pos(Liste,Position,1,Element).
+
+pos([],_,_,Element) :- Element='NULL', fail.
+pos([Kopf|_], Stelle, Stelle, Kopf) :- !.
+pos([_|Rest], Stelle, Iter, Element) :-
+	Next is Iter + 1,
+	pos(Rest,Stelle,Next,Element), !.
+
 
 /* Element hinten anfuegen
  * hintenran(Liste, Elem, Erg).
@@ -47,7 +56,6 @@ ja_nein(Anfrage,Antwort) :-
 	     ;	L == n, Antwort = nein, !
 	     ;	nl, write('Falsche Eingabe.'), nl,
 		fail
-	    
 	    ).
 
 
@@ -81,6 +89,21 @@ loesche_verfeinerung :- retract(verfeinerung(C,A)),fail.
 loesche_verfeinerung.
 
 merke_verfeinerung(Context, Antwort) :- assertz(verfeinerung(Context,Antwort)).
+
+
+:- dynamic moegl_sportarten/1.
+
+/* alle Verfeinerungen in moegl_sportarten/1 loeschen */
+loesche_moegl_sportarten :- retract(moegl_sportarten(S)),fail.
+loesche_moegl_sportarten.
+
+/* zu Beginn sind alle Sportarten unter den Moeglichen */
+init_moegl_sportarten :-
+	loesche_moegl_sportarten,
+	merkmale(S,_,_),
+	assertz(moegl_sportarten(S)),
+	fail.
+init_moegl_sportarten.
 
 
 :- dynamic jaeinzelflag/1.
@@ -130,13 +153,15 @@ interview :-
 	ja_nein(Anfrage,Antwort),
 	(   Antwort == ja,
 	    merkeko(Context,2),
-	    Context == partnersport, set_japartnerflag    
-	;
+	    auswertung_ko(Context),
+	    Context == partnersport, set_japartnerflag
+	 ;
 	    Antwort == nein,
 	    merkeko(Context,1),
+	    auswertung_ko(Context),
 	    (	Context == einzelsport, set_jaeinzelflag
-	    ;	Context == sommersport, set_neinsommerflag
-	    ;	Context == outdoor, set_neinoutdoorflag
+	     ;	Context == sommersport, set_neinsommerflag
+	     ;	Context == outdoor, set_neinoutdoorflag
 	    )
 	),
 	fail.
@@ -152,9 +177,9 @@ zeige_rest.
 
 
 interview_rest :-
-	 frage(Context,Anfrage),
-	 eingabe_loop(Context,Anfrage,Antwort),
-	 merke_verfeinerung(Context,Antwort),
+	frage(Context,Anfrage),
+	eingabe_loop(Context,Anfrage,Antwort),
+	merke_verfeinerung(Context,Antwort),
 	fail.
 interview_rest.
 
@@ -178,26 +203,107 @@ auswahl(Context) :- nl.
 antwort(Context,Antwort) :- option(Context,Antwort,_),!.
 antwort(Context,_) :- write('=>  Antwort ungueltig.'),nl,nl,fail.
 
-run :-
-	loesche_kokriterien,
-	loesche_verfeinerung,
-	interview,
-	zeige_ko,
-	interview_rest,
-	zeige_rest.
 
-run2 :-	loesche_verfeinerung,
-	interview_rest,
-	zeige_rest.
-
-
+auswertung_ko(Context) :-
+	positionen_ko(Context,Pos),
+	moegl_sportarten(Sportart),
+	merkmale(Sportart,Komerkmale,_),
+	pos_in_liste(Komerkmale,Pos,Element),
+	kokriterien(Context, Antwort),
+	(   Antwort == 2, /* ja */
+	    Context == zuhause,
+	    Element == 1,
+	    retract(moegl_sportarten(Sportart)),
+	    writeseq(['LOESCHE alles was nicht zuhause geht: ',Sportart]),nl
+	 ;  Antwort == 1, /* nein */
+	    
+	    /* Sonderfall TEAMSPORT: hier entspricht 3 dem JA */
+	    (	Context==teamsport,
+		Element==3,
+		retract(moegl_sportarten(Sportart)),
+		writeseq(['LOESCHE ',Sportart]),nl
+	    
+	    /* Kein Sport im Sommer => Wintersport => LOESCHE alle Sportarten mit sommer=ja und winter=nein */
+	     ;  Context == sommersport,
+		positionen_ko(wintersport,Poswinter),
+		pos_in_liste(Komerkmale,Poswinter,Winterelem),
+		Element == 2,
+		Winterelem == 1,
+	        retract(moegl_sportarten(Sportart)),
+		writeseq(['LOESCHE ',Sportart]),nl
+	    
+	    /* Kein Sport im Winter => Sommersport => LOESCHE alle Sportarten mit winter=ja und sommer=nein */
+	     ;	Context == wintersport,
+		positionen_ko(sommersport,Possommer),
+		pos_in_liste(Komerkmale,Possommer,Sommerelem),
+		Element == 2,
+		Sommerelem == 1,
+		retract(moegl_sportarten(Sportart)),
+		writeseq(['LOESCHE ',Sportart]),nl
+	    
+	    /* Kein Sport innendrinnen => aussendraussen => LOESCHE alle Sportarten mit indoor=ja und outdoor=nein */
+	     ;  Context == indoor,
+		positionen_ko(outdoor,Posoutdoor),
+		pos_in_liste(Komerkmale,Posoutdoor,Outdoorelem),
+		Element == 2,
+		Outdoorelem == 1,
+	        retract(moegl_sportarten(Sportart)),
+		writeseq(['LOESCHE ',Sportart]),nl
+	    
+	    /* Kein Sport aussendraussen => innendrinnen => LOESCHE alle Sportarten mit outdoor=ja und indoor=nein */
+	     ;	Context == outdoor,
+		positionen_ko(indoor,Posindoor),
+		pos_in_liste(Komerkmale,Posindoor,Indoorelem),
+		Element == 2,
+		Indoorelem == 1,
+		retract(moegl_sportarten(Sportart)),
+		writeseq(['LOESCHE ',Sportart]),nl
+	    
+	    
+	    /* fuer alle uebrigen Sportarten LOESCHE, wenn Eigenschaft vorhanden */
+	     ;	Context \== teamsport,
+		Context \== sommersport,
+		Context \== wintersport,
+		Context \== indoor,
+		Context \== outdoor,
+		Context \== zuhause, /* wird nur bei JA Antwort behandelt */
+	        Element == 2,
+		retract(moegl_sportarten(Sportart)),
+		writeseq(['LOESCHE ',Sportart]),nl
+	    )
+	),
+	fail.
+auswertung_ko(Context).
 
 /*
- * Ausfiltern der Sportarten nach KO-Antworten
- * kofilter(Kriterien,Merkmale,Ergebnis).
- */
-/*
-kofilter([],_,_).
-kofilter([Kkrit|Rkrit],[Kmerk|Rmerk],[Kerg|Rerg]) :-
-	kofilter(Rkrit,Rmerk,Rerg).
+auswertung_schwimmen:
+1) ja:
+moegliche(alle).
+2) nein:
+moegliche(alle\schwimmen).
+
+auswertung_einzelsport:
+1) ja:
+keine veraenderung
+2) nein:
+lese alle moeglichen ein
+entferne alle die einzelsport sind
+
+auswertung_partnersport:
+1) ja: keine veraenderung
+2) nein:
+pruefe jedes der gespeicherten auf reinen partnersport
+   wenn der fall dann retract diesen fakt
+reiner(!) partnersport wird entfernt
 */
+
+init :- loesche_kokriterien,
+	loesche_verfeinerung,
+	init_moegl_sportarten.
+
+run :-
+	init,
+	interview,
+	nl,zeige_ko,
+	interview_rest,
+	nl,zeige_rest.
